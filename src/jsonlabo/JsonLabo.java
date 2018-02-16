23c120b124236d76910ca5933aaabdcc5ad83d3b
Benoit Mignault <b.mignault@gmail.com>
2018-02-16 00:06:51 -0500

    Mise en page

--- src/jsonlabo/JsonLabo.java
+++ src/jsonlabo/JsonLabo.java
@@ -8,40 +8,41 @@ import java.nio.file.Paths;
 public class JsonLabo {
 
     public static void main(String[] args) {
-        String fileJson = StringFromFile("src/jsonlabo/labo.json");
+        String fileJson = StringFromFile("src/jsonlabo/labo.json");        
         // afficher le fichier comme un string simple
-        //System.out.println(fileJson); 
+        // exercice 0
+        System.out.println(fileJson); 
         
-        JSONArray article = new JSONArray(fileJson);
-        
-        
+        JSONArray fichierJson = new JSONArray(fileJson);             
         // exercice 1
-        System.out.println(article.length());
+        System.out.println("Voici la longueur du array : "+fichierJson.length()+"\n");
         
         // exercice 2 affiche info des type film
         System.out.println("Nom des articles de type film :");
-        for (int i = 0; i < article.length(); i++){
+        for (int i = 0; i < fichierJson.length(); i++){
             // afficher les objets
-            JSONObject unArticle = article.getJSONObject(i);
+            JSONObject unArticle = fichierJson.getJSONObject(i);
             
             // equal fait comparer la valeur
             if (unArticle.get("type").equals("film")){
                 System.out.println(unArticle.get("nom"));
             }
         }
+        System.out.println();
         
         // exercice 3 afficher les prix des articles 
         // que les quantites sont plus grand que 0
         
-        for (int i = 0; i < article.length(); i++){
+        for (int i = 0; i < fichierJson.length(); i++){
             // afficher les objets
-            JSONObject unArticle = article.getJSONObject(i);
+            JSONObject unArticle = fichierJson.getJSONObject(i);
             
             // equal fait comparer la valeur
             if (unArticle.getInt("quantite") > 0){
-                System.out.println(unArticle.getString("nom")+" "+unArticle.getFloat("prix"));
+                System.out.println(unArticle.getString("nom")+" : "+unArticle.getFloat("prix"));
             }
-        }        
+        }   
+        System.out.println();
         // exercice 4 creer un objet
         /*
         { accolade est un objet
@@ -57,15 +58,16 @@ public class JsonLabo {
         }
         */        
         JSONObject obj = new JSONObject();
-        JSONObject sousObj1 = new JSONObject();
-        JSONObject sousObj2 = new JSONObject();
-        sousObj1.put("id",125);
-        sousObj2.put("nom","Breaking Bad");       
+        JSONObject articles1 = new JSONObject();
+        JSONObject articles2 = new JSONObject();
+        articles1.put("id",125);
+        articles2.put("nom","Breaking Bad");       
         obj.put("noCommande", 10432);
         obj.put("date", "2014-01-23");
         obj.put("total", 35.99);
-        obj.accumulate("articles", sousObj1);
-        obj.accumulate("articles", sousObj2);
+        obj.put("articles", articles1);
+        // obj.put("articles", articles2); cette action écrase la précédente comme il a le même nom
+        obj.accumulate("articles", articles2);
         
         System.out.print(obj); 
     }
