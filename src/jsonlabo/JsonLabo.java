18daa8ea11227c98d03f57f770aad6e5e1efe3a6
Benoit Mignault <b.mignault@gmail.com>
2018-02-17 22:04:36 -0500

    Ajout exercice 5

--- src/jsonlabo/JsonLabo.java
+++ src/jsonlabo/JsonLabo.java
@@ -1,11 +1,14 @@
 package jsonlabo;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import org.json.*;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.PrintWriter;
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
 
 public class JsonLabo {
 
@@ -70,7 +73,41 @@ public class JsonLabo {
         // obj.put("articles", articles2); cette action écrase la précédente comme il a le même nom
         newJson.accumulate("articles", articles2);
 
-        System.out.print(newJson);
+        System.out.println(newJson);
+
+        /*        
+        Cette fois, créez une commande en y ajoutant un item de chaque article dont le prix est inférieur à 28.00$. Ajuster le total en fonction du prix de chaque article
+         */
+        JSONObject exercice5 = new JSONObject();
+        exercice5.put("noCommande", 0001);
+        exercice5.put("date", "2018-02-15");
+        
+        float prixTotal = 0f;
+        JSONArray articleInferieur = new JSONArray();
+
+        for (int i = 0; i < fichierJson.length(); i++) {
+            // object en preparation de comparaison pour les besoins de la cause
+            JSONObject unArticle = fichierJson.getJSONObject(i);
+
+            if (unArticle.getFloat("prix") < 28.00) {
+                prixTotal += unArticle.getFloat("prix");
+                articleInferieur.put(unArticle);
+            }
+        }
+
+        exercice5.put("total", prixTotal);
+        exercice5.put("articles", articleInferieur);
+
+        try { // la variable true permet d'ajouter des informations
+            FileWriter fw = new FileWriter("src/jsonlabo/new.json"/*,true*/);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);  
+            pw.print(exercice5);
+           
+            pw.close(); bw.close(); fw.close();
+        } catch (IOException ex) {
+            System.out.println("Problème avec l'écriture dans le fichier");
+        }
 
     }
 
@@ -92,7 +129,7 @@ public class JsonLabo {
                 line = br.readLine();
             }
             br.close();
-        } catch (Exception ex) {
+        } catch (IOException ex) {
             System.out.println("Problème d'ouverture du fichier" + "biblio.txt");
         }
 
